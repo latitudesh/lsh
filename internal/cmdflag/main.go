bb66cf98dad9b0dec9009f3ccc1e622c42173bcb
Filipe Nascimento <filipe.nascimento@latitude.sh>
2024-02-14 11:35:35 -0300

    Create a bool attribute for required fields

--- internal/cmdflag/main.go
+++ internal/cmdflag/main.go
@@ -1,6 +1,8 @@
 package cmdflag
 
 import (
+	"fmt"
+
 	"github.com/spf13/pflag"
 )
 
@@ -10,6 +12,15 @@ type FlagSchema struct {
 	Description  string
 	DefaultValue interface{}
 	Type         string
+	Required     bool
+}
+
+func (s *FlagSchema) formattedDescription() string {
+	if !s.Required {
+		return s.Description
+	}
+
+	return fmt.Sprintf("[Required] %v", s.Description)
 }
 
 type FlagsSchema []FlagSchema
@@ -26,15 +37,15 @@ func (f *Flags) Register(s *FlagsSchema) {
 		switch v.Type {
 		case "string":
 			if defaultValue, ok := v.DefaultValue.(string); ok {
-				f.FlagSet.String(v.Name, defaultValue, v.Description)
+				f.FlagSet.String(v.Name, defaultValue, v.formattedDescription())
 			}
 		case "stringSlice":
 			if defaultValue, ok := v.DefaultValue.([]string); ok {
-				f.FlagSet.StringSlice(v.Name, defaultValue, v.Description)
+				f.FlagSet.StringSlice(v.Name, defaultValue, v.formattedDescription())
 			}
 		case "int64":
 			if defaultValue, ok := v.DefaultValue.(int64); ok {
-				f.FlagSet.Int64(v.Name, defaultValue, v.Description)
+				f.FlagSet.Int64(v.Name, defaultValue, v.formattedDescription())
 			}
 		}
 	}
