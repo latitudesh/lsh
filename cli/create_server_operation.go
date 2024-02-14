a100c92ee716b779bc8f2069d367353edb75d4d2
Filipe Nascimento <filipe.nascimento@latitude.sh>
2024-02-14 11:49:14 -0300

    Allow to skip the RAID level option

--- cli/create_server_operation.go
+++ cli/create_server_operation.go
@@ -153,7 +153,6 @@ func (o *CreateServerOperation) run(cmd *cobra.Command, args []string) error {
 	operation.AssignBodyAttributes(o, params.Body.Data.Attributes)
 
 	if dryRun {
-
 		logDebugf("dry-run flag specified. Skip sending request.")
 		return nil
 	}
