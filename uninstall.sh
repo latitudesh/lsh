758136ffcf44d4e39fbc265a6891596df4e9cc7a
Yuri Hummel <yurihummel@outlook.com>
2024-02-23 13:57:59 -0300

    chore: adjustments to script

--- uninstall.sh
+++ uninstall.sh
@@ -12,31 +12,31 @@ echo -e "Removing $INSTALL_DIR from PATH\n";
 
 if echo $PATH | grep -q $INSTALL_DIR 
 then
+NEW_PATH=$(echo $PATH | sed 's|:'$HOME_DIR/.lsh'||g');
 case "$SHELL_NAME" in
     "bash")
         SHELL_CONFIG_PATH=~/.bashrc
-        NEW_PATH=$(echo $PATH | sed 's|:'$HOME_DIR/.lsh'||');
         echo "export PATH=$NEW_PATH" >> $SHELL_CONFIG_PATH;
         ;;
     "zsh")
         SHELL_CONFIG_PATH=~/.zshrc
-        NEW_PATH=$(echo $PATH | sed 's|:'$HOME_DIR/.lsh'||')
         echo "export PATH=$NEW_PATH" >> $SHELL_CONFIG_PATH
         ;;
     "fish")
         SHELL_CONFIG_PATH=~/.config/fish/config.fish
-        NEW_PATH=$(echo $PATH | sed 's|:'$HOME_DIR/.lsh'||')
         echo 'set -gx PATH $NEW_PATH' >> $SHELL_CONFIG_PATH
         ;;
     *)
         echo "Unsupported shell: $SHELL_NAME, you will need to set your PATH manually\n"
         ;;
 esac
-else
-    echo -e "lsh executable not in PATH\n"
-fi
 
 echo -e "Deleting $INSTALL_DIR\n";
 rm -dr $INSTALL_DIR;
 
-echo -e "Uninstall finished successfully\n"
+echo -e "Uninstall finished successfully\nrun: \n"
+echo -e "    source $SHELL_CONFIG_PATH\n"
+echo -e "to apply changes to your current session"
+else
+    echo -e "lsh executable not found!\n"
+fi
